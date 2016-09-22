28e8dde7b807a20cf44b5a5e3fc02489673aae97
Alauridsen <andrslauridsen@gmail.com>
2016-09-22 15:41:22 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,9 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
 
-<body>
-</body>
-</html>
+<?php 
+	  $curpage = basename($_SERVER['PHP_SELF']);
+?>
+<ul id="menu">
+	<li><a href="index.php" <?PHP if ($curpage == 'index.php') {echo 'class="active"';}?>>Side 1</a></li>
+    <li><a href="p2.php" <?PHP if ($curpage == 'p2.php') {echo 'class="active"';}?>>Side 2</a></li>
+    <li><a href="p3.php" <?PHP if ($curpage == 'p3.php') {echo 'class="active"';}?>>Side 3</a></li>
+</ul>
