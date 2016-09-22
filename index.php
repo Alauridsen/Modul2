c8dc07db3499f705990698b2e32dfbf7883aee0a
Alauridsen <andrslauridsen@gmail.com>
2016-09-22 15:40:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
+<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+<?PHP include 'menu.php'; ?>
+<h1>Side 1</h1>
+<p>text</p>
 </body>
 </html>
+
