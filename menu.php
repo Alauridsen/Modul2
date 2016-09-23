7d5cb7d21c256baf8cef02ec625f9f2ebeb5da86
Alauridsen <andrslauridsen@gmail.com>
2016-09-23 09:04:58 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,8 @@
-
 <?php 
+	/*Objekt der finder nuværende sides filnavn. */
 	  $curpage = basename($_SERVER['PHP_SELF']);
 ?>
+<!-- If clausul der echo'er class'en active i det nuværende menu link. -->
 <ul id="menu">
 	<li><a href="index.php" <?PHP if ($curpage == 'index.php') {echo 'class="active"';}?>>Side 1</a></li>
     <li><a href="p2.php" <?PHP if ($curpage == 'p2.php') {echo 'class="active"';}?>>Side 2</a></li>
